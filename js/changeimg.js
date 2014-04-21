c475bb81a28d647e148af9794c34f94ad94dcdbd
wkmkymt <wkmkymt@gmail.com>
2014-04-22 01:58:12 +0900

    Add event when touch menu

--- js/changeimg.js
+++ js/changeimg.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("nav ul li a img").on("mouseover touchstart", function() {
 		$(this).attr("src", $(this).attr("src").replace("of", "on"));
 	});
-	$("nav ul li a img").on("mouseout touchend", function() {
+	$("nav ul li a img").on("mouseout touchend touchmove touchcancel", function() {
 		$(this).attr("src", $(this).attr("src").replace("on", "of"));
 	});
 });
